de603d118cdda4aba5b4430c5b592c9685c039ec
gunel2707 <gunel.muradova2707@gmail.com>
2024-07-25 18:05:07 +0400

    update

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,6 @@
-import React ,{useContext} from 'react'
+import React, { useContext } from "react";
 import Context from "../context/Context";
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
 import "../pages/Product.css";
 const ProductDetails = () => {
   const {
@@ -12,18 +12,31 @@ const ProductDetails = () => {
     modalshow,
     setmodalshow,
   } = useContext(Context);
-  const {id}=useParams()
-  const findProduct=productsData.find(product=>product.id==id)
+  const { id } = useParams();
+  const findProduct = productsData.find((product) => product.id == id);
   console.log(findProduct);
   console.log(id);
   return (
-    <div className='Productdetailcontent'><img className='Productdetailimage' src={findProduct.image} alt="" />
-<h4>Category:{findProduct.category}</h4> 
- <h1>Price:{findProduct.price}</h1>
-<p><b>Title:{findProduct.title}</b></p> 
- <p>Description:{findProduct.description}</p>
-    </div>
-  )
-}
+    <>
+      {findProduct ? (
+        <>
+          <div className="Productdetailcontent">
+            <img
+              className="Productdetailimage"
+              src={findProduct.image}
+              alt=""
+            />
+            <h4>Category:{findProduct.category}</h4>
+            <h1>Price:{findProduct.price}</h1>
+            <p>
+              <b>Title:{findProduct.title}</b>
+            </p>
+            <p>Description:{findProduct.description}</p>
+          </div>
+        </>
+      ) : null}
+    </>
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
